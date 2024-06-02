61772be133b1d53a7a846c5fa73cfb9f53d7442c
gelvetica <helveticahelvetica@protonmail.com>
2024-06-02 17:09:21 -0400

    cleanup bc pycharm told me i suck

--- src/app.py
+++ src/app.py
@@ -1,91 +1,96 @@
 from playwright.sync_api import sync_playwright
 import requests
 import re
-import urllib
-import json
+import urllib.parse
 import os
 import yaml
-import time
-import datetime
 from flask import Flask
 from flask import redirect
 
 app = Flask(__name__)
 
-
 datadir = os.environ["DATADIR"]
 with open(os.path.join(datadir, "config.yml"), 'r') as file:
     config = yaml.safe_load(file)
 
+
 class TokenError(Exception):
     pass
+
+
 class BlockedChannelError(Exception):
     pass
 
-class Tokensniffer:
+
+class TokenSniffer:
     def __init__(self, page):
         self.token = []
         self.page = page
+
+    # noinspection PyTypeChecker
+    # Pycharm thinks self.on_request is being incorrectly used with page.goto; Not sure why
     def refresh(self):
         with sync_playwright() as p:
             browser = p.firefox.launch(headless=True)
             page = browser.new_page()
-            page.on("request", self.onRequest)
+            page.on("request", self.on_request)
             page.goto(urllib.parse.urljoin(config["tv_url"], self.page + "/"))
             browser.close()
-    def onRequest(self, request):
-        streamregex = "/([^\_]*).m3u8"
+
+    def on_request(self, request):
+        stream_regex = "/([^_]*).m3u8"
         print(request.url)
-        if re.search(streamregex, request.url) != None:
+        if re.search(stream_regex, request.url) is not None:
             self.token.append(request.url)
             print("new token: " + request.url)
         else:
             return False
 
-def listChannels():
+
+def list_channels():
     res = {}
-    channelListRegex = "<a class=\"list-group-item\" href=\"(.*)/\">(.*)</a>"
-    indexHTML = requests.get(config["tv_url"]).text
-    for line in indexHTML.splitlines():
-        search = re.search(channelListRegex, line)
-        if search == None:
+    channel_list_regex = "<a class=\"list-group-item\" href=\"(.*)/\">(.*)</a>"
+    index_html = requests.get(config["tv_url"]).text
+    for line in index_html.splitlines():
+        search = re.search(channel_list_regex, line)
+        if search is None:
             continue
         name = search.group(2)
         url = search.group(1)
         res[name.replace("&amp;", "&")] = url
     # event processing
-    eventTypeRegex = "            <h3>(.*)</h3>"
-    eventListRegex = "                                            <a class=\"list-group-item\" href=\"(.*)/\">(.*)"
-    currentType = None
-    currentNum = 0
-    for line in indexHTML.splitlines():
-        search = re.search(eventTypeRegex, line)
-        if search == None:
-            search = re.search(eventListRegex, line)
-            if search == None:
+    event_type_regex = "            <h3>(.*)</h3>"
+    event_list_regex = "                                            <a class=\"list-group-item\" href=\"(.*)/\">(.*)"
+    current_type = None
+    current_num = 0
+    for line in index_html.splitlines():
+        search = re.search(event_type_regex, line)
+        if search is None:
+            search = re.search(event_list_regex, line)
+            if search is None:
                 continue
-            currentNum += 1
-            res[currentType + " Event " + str(currentNum)] = search.group(1)
+            current_num += 1
+            res[current_type + " Event " + str(current_num)] = search.group(1)
             continue
         print(search.group(1))
-        currentType = search.group(1)
-        currentNum = 0
-
-
+        current_type = search.group(1)
+        current_num = 0
 
     return res
 
-def getStreamUrl(page):
-    token = Tokensniffer(page)
+
+def get_stream_url(page):
+    token = TokenSniffer(page)
     token.refresh()
     return token.token
 
-def getStream(page):
+
+def get_stream(page):
     if page not in channels.values():
         raise KeyError()
     if page not in streams.keys():
-        streams[page] = getStreamUrl(page)
-    if streams[page] == None:
+        streams[page] = get_stream_url(page)
+    if streams[page] is None:
         raise BlockedChannelError
     r3 = requests.get(streams[page][0])
     if r3.status_code != 200:
@@ -96,32 +101,33 @@ def getStream(page):
         if "_high" in line:
             return urllib.parse.urljoin(streams[page][0], line)
     return streams[page][0]
-#token = Tokensniffer("fox-news-channel-live-stream")
-#token.refresh()
+
+
 events = {}
-channels = listChannels()
+channels = list_channels()
 streams = {}
 
 
-
-
 @app.route("/channels.m3u")
-def fullm3u():
+def channels_m3u():
     m3u = "#EXTM3U"
     for i, value in channels.items():
-        m3u += "\n#EXTINF:-1 tvg-chno=\"%s\",%s\n%s/channel%s" % (str(list(channels.keys()).index(i) + 1), i, config["visible_url"], value)
-    return(m3u)
+        m3u += "\n#EXTINF:-1 tvg-chno=\"%s\",%s\n%s/channel%s" % (
+            str(list(channels.keys()).index(i) + 1), i, config["visible_url"], value)
+    return m3u
+
+
 @app.route("/channel/<path:channel>")
-def appchannel(channel):
+def app_channel(channel):
     channel = "/" + channel
     try:
-        res = getStream(channel)
+        res = get_stream(channel)
         return redirect(res)
     except TokenError:
-        streams[channel] = getStreamUrl(channel)
-        res = getStream(channel)
+        streams[channel] = get_stream_url(channel)
+        res = get_stream(channel)
         return redirect(res)
     except BlockedChannelError:
-        return ("Channel is blocked", 403) 
+        return "Channel is blocked", 403
     except KeyError:
-        return ("Channel does not exist", 404)
+        return "Channel does not exist", 404
